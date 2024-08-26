dff3864ea5cc5bbec1f0ac647e228d2ac27578a1
HieuHT <varenty02@gmail.com>
2024-08-26 07:07:00 +0700

    feat: storage authentication

--- middleware/authenticate.go
+++ middleware/authenticate.go
@@ -2,14 +2,26 @@ package middleware
 
 import (
 	"code-math/component/tokenprovider"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+func extractTokenFromHeaderString(s string) (string, error) {
+	parts := strings.Split(s, " ")
+	if parts[0] != "Bearer" || len(parts) < 2 || strings.TrimSpace(parts[1]) == "" {
+		return "", fmt.Errorf("error cannot extract")
+	}
+	return parts[1], nil
 
+}
 func AuthMiddleware(secretKey string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
+		tokenString,err := extractTokenFromHeaderString(c.GetHeader("Authorization"))
+		if err!=nil{
+			panic(err) 
+		}
 		if tokenString == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "authorization header missing"})
 			c.Abort()
